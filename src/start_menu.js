abcffe520879d4fc9258061292b4170a9cf9ab77
Brennan <flood.brennan@gmail.com>
2020-01-21 19:05:24 -0500

    update start menu

--- src/start_menu.js
+++ src/start_menu.js
@@ -3,22 +3,26 @@ function StartMenu() {
 };
 
 StartMenu.prototype.draw = function draw(ctx) {
-  ctx.font = "40px Open Sans";
+  ctx.font = "50px Open Sans";
   ctx.fillStyle = "gold";
   ctx.textAlign = "center";
   ctx.fillText("Demon Slayer", 700 / 2, 480 / 3);
+  ctx.font = "18px Open Sans";
+  ctx.fillStyle = "white";
+  ctx.textAlign = "center";
+  ctx.fillText("Survive for as long as you can!", 700 / 2, 400 / 2);
   ctx.font = "16px Open Sans";
-  ctx.fillStyle = "gold";
+  ctx.fillStyle = "white";
   ctx.textAlign = "center";
   ctx.fillText("-Use 'W,A,S,D' to move, 'space' to attack, and 'shift' to dash", 700 / 2, 480 / 2);
-  ctx.font = "16px Open Sans";
+  ctx.font = "20px Open Sans";
   ctx.fillStyle = "gold";
   ctx.textAlign = "center";
-  ctx.fillText("-Press 'return' to start playing, or reset at any time", 700 / 2, 540 / 2);
+  ctx.fillText("Press 'return' to start", 700 / 2, 330);
   ctx.font = "16px Open Sans";
-  ctx.fillStyle = "gold";
+  ctx.fillStyle = "white";
   ctx.textAlign = "center";
-  ctx.fillText("-Press 'esc' to return to the start menu", 700 / 2, 600 / 2);
+  ctx.fillText("-Press 'esc' to return to the main menu", 700 / 2, 540 / 2);
 };
 
 StartMenu.prototype.move = function move() {
